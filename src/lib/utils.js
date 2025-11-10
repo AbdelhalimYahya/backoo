085a1071d2bd87d017a9680a8d29602433c4a7f6
abdelhalim <abdelhalim@gmail.com>
2025-11-11 01:26:21 +0200

    alk;sfjda;sf

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,11 +3,27 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userID , res) => {
     const token = jwt.sign({userID} , process.env.JWT_SECRET , {expiresIn : "15d"});
     res.cookie("token" , token , {
-        httpOnly : true, // more secure
-        secure : process.env.NODE_ENV !== "development",
-        sameSite : "strict", // more secure
-        maxAge : 15 * 24 * 60 * 60 * 1000
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+        partitioned: true,
     });
 
     return token;
 };
+
+
+// import jwt from "jsonwebtoken";
+
+// export const generateToken = (userID , res) => {
+//     const token = jwt.sign({userID} , process.env.JWT_SECRET , {expiresIn : "15d"});
+//     res.cookie("token" , token , {
+//         httpOnly : true, // more secure
+//         secure : process.env.NODE_ENV !== "development",
+//         sameSite : "strict", // more secure
+//         maxAge : 15 * 24 * 60 * 60 * 1000
+//     });
+
+//     return token;
+// };
