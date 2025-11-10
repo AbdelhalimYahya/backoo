e9434603393d85f001b2979525276ff485ce0238
abdelhalim <abdelhalim@gmail.com>
2025-11-11 00:31:42 +0200

    asldfkjaslfdkja

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const __dirname = path.resolve();
 // app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,8 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: ["http://localhost:5173"],
   },
 });
 
