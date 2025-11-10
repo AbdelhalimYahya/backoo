06a9d0d4adf4bc376acdc48f677cc99f5e66fae3
abdelhalim <abdelhalim@gmail.com>
2025-11-11 01:34:46 +0200

    aslk;dfja;sjkdf

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,8 +4,8 @@ export const generateToken = (userID , res) => {
     const token = jwt.sign({userID} , process.env.JWT_SECRET , {expiresIn : "15d"});
     res.cookie("token" , token , {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 1000 * 60 * 60 * 24 * 30,
         partitioned: true,
     });
